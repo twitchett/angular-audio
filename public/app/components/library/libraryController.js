3c2ba8f72340de08282a5e3273852c26dd5af954
twitchett <tg.twitchett@gmail.com>
2015-09-27 11:37:23 +0200

    initial front-end library functionality (rough)
    - displaying data in table
    - adding filters, list/extended display options

--- public/app/components/library/libraryController.js
+++ public/app/components/library/libraryController.js
@@ -6,17 +6,34 @@
 	* Library Controller
 	*/
 	function LibraryController($log, $scope, $modal, libraryService) {
+		console.log('in LibraryController')
 
 		var log	= $log.getInstance('LibraryController'),
 			vm	= this;
 
-		vm.animationsEnabled = true;
+		var filters = {
+			src : {
+				yt : true,
+				sc : true
+			},
+			tags 	: []
+		}
 
-		console.log('LC calling GL wht');
+		console.log('LC calling GL whaaat');
 		getLibrary();
 
+		// state
+		vm.filters = filters;
+		vm.order = 'name';
+		vm.search = '';
+		vm.expanded = false;
+		vm.animationsEnabled = true;
+
+		// behaviour
 		vm.getLibrary = getLibrary;
 		vm.openImport = openImport;
+		vm.selectTrack = selectTrack;
+		vm.expand = expand;
 
 		function getLibrary() {
 			libraryService.getLibrary().then(function(data) {
@@ -28,6 +45,12 @@
 			});
 		}
 
+		function selectTrack($index, $event) {
+			var track = vm.tracks[$index];
+			console.log('selected: ', track);
+			//video.selected = !video.selected; 		---- testing
+			if (track) track.selected = true;
+		}
 		function openImport() {
 			var modalInstance = $modal.open({
 				animation 	: true,
@@ -42,25 +65,48 @@
 			});
 
 			modalInstance.result.then(function (data) {
-					$log.info('Modal dismissed at: ' + new Date());
-					vm.tracks = getLibrary();
-				},
-				function () {
-					vm.tracks = getLibrary();
-					$log.info('Modal dismissed (with error?) at: ' + new Date());
+				$log.info('Modal dismissed at: ' + new Date());
+				libraryService.getLibrary(true).then(function(tracks) {
+					vm.tracks = tracks;
 				});
+			},
+			function (error) {
+				$log.info('Modal dismissed with error ', error);
+				libraryService.getLibrary(true).then(function(tracks) {
+					vm.tracks = tracks;
+				});
+			});
 		};
+
+		function expand(expand) {
+			vm.expanded = expand;
+			console.log('expanded: ' + vm.expanded)
+		}
+	}
+
+	function TypeFilter() {
+		return function(tracks, filters) {
+			var filtered = [];
+
+			angular.forEach(tracks, function(track, idx) {
+				if (filters.src[track.src] === true) {
+					filtered.push(track);
+				}
+			});
+
+			return filtered;
+		}
 	}
 
 	// angular config
 	angular
-		.module('app.library')
+		.module('app')
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/library', {
 				templateUrl: 'app/components/library/library.html',
 				controller: 'LibraryController'
 			});
 		}])
-		.controller('LibraryController', ['$log', '$scope', '$modal', 'LibraryService', LibraryController]);
-
+		.controller('LibraryController', ['$log', '$scope', '$modal', 'LibraryService', LibraryController])
+		.filter('typeFilter', TypeFilter);
 })();
